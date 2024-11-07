5d4ac30d9e2ade42a996c682939f3b7adfb995b3
groe83 <go.opazo@duocuc.cl>
2024-11-07 12:16:15 -0300

    Definir BASE_DIR y configurar STATIC_ROOT

--- modelos_predictivos/settings.py
+++ modelos_predictivos/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
